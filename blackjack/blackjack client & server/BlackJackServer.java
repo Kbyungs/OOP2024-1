439b7f55b8db028bf2890ceccbf643fd2ac2ced1
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-04-08 23:12:40 +0900

    Update BlackJackServer.java 0408
    J, Q, K -> 10
    A can be 1 or 11

--- blackjack/blackjack client & server/BlackJackServer.java
+++ blackjack/blackjack client & server/BlackJackServer.java
@@ -1,5 +1,5 @@
 // Todo list
-// 1. J,Q,K
+// 1. J,Q,K -> clear
 // 2. improve dealer strategy
 // 3. display history
 // 4. split, insurance
@@ -24,10 +24,11 @@ class BlackJackServer {
     }
 
     public void createServer() throws Exception {
-        System.out.println("Server start running ..");
+        System.out.println("강병수 카지노 작동중....");
         ServerSocket server = new ServerSocket(inPort);
         while (true) {
             Socket socket = server.accept();
+            //2번 : 서버 열기
             Client c = new Client(socket);
             clients.add(c);
         }
@@ -36,7 +37,11 @@ class BlackJackServer {
     public int getCard() {
         Random random = new Random();
         int newcard = random.nextInt(13)+1;
-        System.out.println("new card is "+newcard);
+        if (newcard > 1 && newcard < 11) System.out.println("new card is "+newcard);
+        if (newcard == 1) System.out.println("new card is A");
+        if (newcard == 11) System.out.println("new card is JACK");
+        if (newcard == 12) System.out.println("new card is QUEEN");
+        if (newcard == 13) System.out.println("new card is KING");
         return newcard;
     }
 
@@ -46,9 +51,10 @@ class BlackJackServer {
         PrintWriter out = null;
         BufferedReader in = null;
         int card;
+        boolean hadAce = false;
 
         public Client(Socket socket) throws Exception {
-            System.out.println("\n\n"+socket.getInetAddress()+ "  join ");
+            System.out.println("\n\n"+socket.getInetAddress()+ "  join "); //3번
             this.socket = socket;
             card = 0;
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -56,8 +62,22 @@ class BlackJackServer {
             start();
 
             int newCard = getCard();
-            card += newCard;
-            send(""+newCard);
+            if (newCard > 10) {
+                if (newCard == 11) System.out.println("new card is JACK");
+                if (newCard == 12) System.out.println("new card is QUEEN");
+                if (newCard == 13) System.out.println("new card is KING");
+                card += 10;
+            } else {
+                if (newCard == 1) {
+                    System.out.println("new card is A");
+                    hadAce = true;
+                }
+                else System.out.println("new card is "+newCard);
+                card += newCard;
+                if (hadAce && card + 10 <= 21) card += 10;
+            }
+//            card += newCard;
+            send(""+newCard); //4번 client에게 카드 주는거
             System.out.println(socket.getInetAddress()+ "  has "+ card);
 
         }
@@ -74,23 +94,26 @@ class BlackJackServer {
 
             try {
                 while(true) {
-                    msg = in.readLine();
+                    msg = in.readLine(); //6번
 
                     if (msg.equalsIgnoreCase("1")) {
                         System.out.println(socket.getInetAddress()+" hit");
                         newcard = getCard();
-                        card += newcard;
+                        if (newcard > 10) card += 10;
+                        else card += newcard;
                         System.out.println(socket.getInetAddress()+" has "+card);
-                        send(""+newcard);
+                        send(""+newcard); //7번
 
                         if (card > 21) {
                             System.out.println(socket.getInetAddress()+" has "+card+", Over 21! \n\n");
                             card = getCard();
+                            hadAce = false;
                             send(""+card);
                         }
                         if (card == 21) {
                             System.out.println(socket.getInetAddress()+" has "+card+", BLACKJACK! \n\n");
                             card = getCard();
+                            hadAce = false;
                             send(""+card);
                         }
 
@@ -104,12 +127,15 @@ class BlackJackServer {
 
                         if ((card > dealer) || (dealer > 21)) {
                             System.out.println(socket.getInetAddress()+ " win!\n\n");
+                            hadAce = false;
                         }
                         else if (card < dealer) {
                             System.out.println("Dealer win!\n\n");
+                            hadAce = false;
                         }
                         else {
                             System.out.println("tie! \n\n");
+                            hadAce = false;
                         }
 
                         card = getCard();
