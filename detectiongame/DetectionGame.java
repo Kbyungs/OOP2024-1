d51a32f10a62941c44a0de1aa2c57d1ae0fd1dfa
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-04-15 22:29:43 +0900

    comment, num of left mines

--- detectiongame/DetectionGame.java
+++ detectiongame/DetectionGame.java
@@ -1,70 +1,64 @@
 // Enjoy mine game
 // dr.seungchul@gmail.com
-
+/*
+<To Do List>
+- 주변 위치 밝히기?
+    -> 하나 고르면 주변 폭탄 갯수 알려주기?
+*/
 import java.util.*;
 
 public class DetectionGame {
-	public static Map map;
-	public static int width;
-	public static int num_mine;
-	
-	public static void main(String []args){
-		int find=0;
-		initial();
-		
-		while(find<num_mine) 
-			find += guess();
-					
-		System.out.println("Success found "+find+" mines !");
-		
-	}
-	
-	private static void initial() {
-		System.out.println("Welcome to the Mine Game !");
-		Scanner scan = new Scanner (System.in);
-		
-		
-		System.out.print("Enter the width (1~10, width x width) :");
-		width = scan.nextInt();
-		while ((width < 1) || (width>10)) {
-			System.out.println("Invalid width, enter 1~10 !");
-			width = scan.nextInt();
-		}
-		
-		System.out.print("Enter number of mines : ");
-		num_mine = scan.nextInt();
-		while ((num_mine >= width * width) || (num_mine< 1)) {
-			System.out.println("Invalid number of mines, must be 0 ~ " + width * width);
-			num_mine = scan.nextInt();
-		}	
-		
-		map = new Map(width, num_mine);		
-	}
-	
-	public static int guess() {
-		Scanner scan = new Scanner (System.in);
-		System.out.print(" Enter x coordinate(0 ~ "+width+") :");
-		int x = scan.nextInt();
-		while ((x < 0) || (x >= width)) {
-			System.out.println(" Invalid x, enter a new x coordinate");
-			x = scan.nextInt();
-		}
-		System.out.print(" Enter y coordinate(0 ~ "+width+") :");
-		int y = scan.nextInt();
-		while ((y < 0) || (y >= width)) {
-			System.out.println(" Invalid y, enter a new y coordinate");
-			y = scan.nextInt();
-		}
-		
-		if (map.checkMine(x,y) >= 0) {			
-			map.updateMap(x,y);
-			return 1;
-		}
-		else 
-			return 0;
-	}
-	
-		
-		
-		
+    public static Map map;
+    public static int width;
+    public static int num_mine;
+
+    public static  void main(String[] args) {
+        int find = 0;
+        initial();
+        //다 찾을 때까지 무한 반복
+        while (find < num_mine) find += guess();
+        System.out.println("All mines you found!");
+    }
+
+    private static void initial() { //초기 입력 받고 기본적인 map setting
+        System.out.println("I My Me \"Mine\" Game !");
+        Scanner sc = new Scanner(System.in);
+        //map 크기 설정
+        System.out.print("Enter the width (1 ~ 10, width x width): ");
+        width = sc.nextInt();
+        while ((width < 1) || (width > 10)) {
+            System.out.println("Invalid width, enter 1~10");
+            width = sc.nextInt();
+        }
+        //mine 갯수 설정
+        System.out.print("Enter number of mines :");
+        num_mine = sc.nextInt();
+        while ((num_mine >= width*width) || (num_mine < 1)) {
+            System.out.println("Invalid number of mines, must be 0 ~ " +width*width);
+            num_mine = sc.nextInt();
+        }
+        //Map파일에서 map클래스 새로 선언(설정한대로)
+        map = new Map(width, num_mine);
+    }
+
+    public static int guess() { //한 칸씩 찍어보기 범위 틀리면 말해주고, checkMine, updateMine함수로 연결
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter Row coordinate(0 ~ "+width+") :");
+        int r = sc.nextInt();
+        while ((r < 0) || (r >= width)) {
+            System.out.println(" Invalid Row, enter a new Row coordinate");
+            r = sc.nextInt();
+        }
+        System.out.print(" Enter Column coordinate(0 ~ "+width+") : ");
+        int c = sc.nextInt();
+        while ((c < 0) || (c >= width)) {
+            System.out.println(" Invalid Column, enter a new Column coordinate");
+            c = sc.nextInt();
+        }
+
+        if (map.checkMine(r, c) >= 0) {
+            map.updateMap(r, c);
+            return 1;
+        } else return 0;
+    }
 }
