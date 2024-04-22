ca3d1fc56ef13bf6012a428365e27aaeebaf3cb7
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-04-22 16:30:41 +0900

    Update Team.java

--- Sports/Team.java
+++ Sports/Team.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Team {
     public static int num_teams=0;
     public String name;
-    public int rank=0;
+    public int rank = 0;
     public Player captain;
     public ArrayList<Player> members= new ArrayList<>();
 
@@ -11,43 +11,43 @@ public class Team {
         num_teams++;
     }
 
-    public Team(String n) {
-        name = n;
+    public Team(String name) {
+        this.name = name;
         num_teams++;
     }
 
-    public Team(String n, int r) {
-        name = n;
-        rank = r;
+    public Team(String name, int rank) {
+        this.name = name;
+        this.rank = rank;
         num_teams++;
     }
 
-    public Team(String n, int r, Player p) {
-        name = n;
-        rank = r;
+    public Team(String name, int rank, Player player) {
+        this.name = name;
+        this.rank = rank;
         num_teams++;
-        members.add(p);
+        members.add(player);
     }
 
-    public void setName(String n) { name = n; }
+    public void setName(String name) { this.name = name; }
 
     public String getName() { return name; }
 
-    public void setRank(int r) { rank = r; }
+    public void setRank(int rank) { this.rank = rank; }
 
     public int getRank() { return rank; }
 
     public int getSize() { return members.size(); }
 
-    public void setCaptain(Player p) { captain = p; }
+    public void setCaptain(Player player) { captain = player; }
 
     public Player getCaptain() { return captain; }
 
-    public void addMember(Player p) { members.add(p); }
+    public void addMember(Player player) { members.add(player); }
 
-    public void removeMember(String n) {
+    public void removeMember(String name) {
         for (int i = getSize()-1; i >= 0; i--) {
-            if (members.get(i).name.equals(n)) members.remove(i);
+            if (members.get(i).name.equals(name)) members.remove(i);
         }
     }
 
