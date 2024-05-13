d81ef399755b4ccb77137766051017cf9fa21e0a
KBSOO <86033302+Kbyungs@users.noreply.github.com>
2024-05-13 10:23:47 +0900

    Update MineClientGUI.java

--- MineGUI/MineClientGUI/MineClientGUI.java
+++ MineGUI/MineClientGUI/MineClientGUI.java
@@ -3,40 +3,38 @@
 // 1. statistic (#success, #fail)
 // 2. prevent same trial
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 
 class MineClientGUI extends JFrame {
     static int inPort = 9999;
-    static String address ="192.168.0.6";
-    static public PrintWriter out;
-    static public BufferedReader in;
+    static String address ="192.168.0.23";
+        static public PrintWriter out;
+        static public BufferedReader in;
     static int width=0;
     static int num_mine=0;
     static Map map;
-    static int find=0;
+    static int find = 0;
+    static int totalTry = 0;
     static public Socket socket;
 
     public Container cont;
-    public JPanel p0, p1;
+    public JPanel p0, p1, p2, p3;
     public JSlider slider0, slider1;
-    public JButton b_map;
+    public JButton b_map, b_retry;
+
+    public JLabel l_try, l_success, l_left;
     public JButton[] buttons;
+
+    public ImageIcon icon = new ImageIcon("../img/marioQbox.jpeg");
 
 
     public static void main(String[] args) {
@@ -49,11 +47,24 @@ class MineClientGUI extends JFrame {
         setLocation(150, 150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         cont = getContentPane();
-        cont.setLayout(new FlowLayout());
+        cont.setLayout(new BoxLayout(cont, BoxLayout.Y_AXIS)); // 상하로 차곡차곡 배치되도록 설정
+
         p0 = new JPanel();
-        p0.setBackground(Color.CYAN);
+        p0.setBackground(Color.lightGray);
+        p0.setLayout(new FlowLayout()); // p0는 FlowLayout으로 설정
+
         p1 = new JPanel();
-        p1.setBackground(Color.YELLOW);
+        p1.setBackground(Color.lightGray);
+//        p1.setLayout(new FlowLayout()); // p1도 FlowLayout으로 설정
+        p1.setLayout(new GridLayout(1,1));
+
+        p2 = new JPanel();
+        p2.setBackground(Color.lightGray);
+        p2.setLayout(new FlowLayout()); // p2도 FlowLayout으로 설정
+
+        p3 = new JPanel();
+        p3.setBackground(Color.lightGray);
+        p3.setLayout(new FlowLayout()); // p2도 FlowLayout으로 설정
 
         slider0 = new JSlider(JSlider.HORIZONTAL, 1, 10, 5);
         slider0.setMinorTickSpacing(1);
@@ -63,17 +74,45 @@ class MineClientGUI extends JFrame {
         slider0.addChangeListener(new MyChangeListener());
         slider1 = new JSlider();
         slider1.addChangeListener(new MyChangeListener());
+
         b_map = new JButton("Create Map");
+        b_map.setBackground(Color.YELLOW);
+//        b_map.setIcon(icon);
         b_map.addActionListener(new MyActionListener0());
+
+        b_retry = new JButton("RETRY");
+        b_retry.addActionListener(new MyActionListener2());
+        l_success = new JLabel("find : " +find);
+        l_try = new JLabel("try : " + totalTry);
+
+        l_left = new JLabel("\"" +(num_mine-find)+"\" mines left");
+        l_left.setFont(new Font("Arial", Font.PLAIN, 20));
 
         p0.add(slider0);
         p0.add(slider1);
         p0.add(b_map);
+
+        p1.add(l_success);
+        p1.add(l_try);
+
+        p2.add(l_try);
+        p2.add(l_success);
+        p2.add(b_retry);
+
+        p3.add(l_left);
+
+        cont.add(p2);
+        cont.add(p3);
         cont.add(p0);
         cont.add(p1);
         pack();
+
         setVisible(true);
+        p1.setVisible(false);
+        p2.setVisible(false);
+        p3.setVisible(false);
     }
+
 
 
     class MyChangeListener implements ChangeListener {
@@ -93,28 +132,39 @@ class MineClientGUI extends JFrame {
     }
 
 
-    class MyActionListener0 implements ActionListener {
+    class MyActionListener0 implements ActionListener { //create Map
         public void actionPerformed(ActionEvent e) {
             try {
                 socket = new Socket(address, inPort);
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out.println(width+","+num_mine);
-            }
-            catch (Exception e1) {}
+            } catch (Exception e1) {}
 
+            p1.removeAll();
+
+            // 새로운 맵 객체 생성
             map = new Map(width, num_mine);
 
+            l_left.setText("\"" +(num_mine-find)+"\" mines left");
             p1.setLayout(new GridLayout(width, width));
             buttons = new JButton[width*width];
             for (int i=0; i<width*width; i++) {
                 buttons[i] = new JButton(""+i);
+                buttons[i].setForeground(Color.white);
+//                buttons[i] = new JButton();
+                buttons[i].setIcon(icon);
+                buttons[i].setPreferredSize(new Dimension(30, 30));
                 buttons[i].addActionListener(new MyActionListener1());
                 p1.add(buttons[i]);
             }
-
+            p0.setVisible(false);
+            p1.setVisible(true);
+            p2.setVisible(true);
+            p3.setVisible(true);
         }
     }
+
 
     class MyActionListener1 implements ActionListener {
         public void actionPerformed(ActionEvent e) {
@@ -130,21 +180,42 @@ class MineClientGUI extends JFrame {
                 if (result>=0) {
                     //System.out.println("   Find mine at ("+x+", "+y+")");
                     JButton b = (JButton)e.getSource();
-                    b.setText("O");
+                    b.setText(result+"");
+                    b.setForeground(Color.black);
                     map.updateMap(x,y);
+                    totalTry++;
+                    l_try.setText("try : " + totalTry);
                 }
                 else {
                     //System.out.println("   No mine at ("+x+", "+y+")");
                     JButton b = (JButton)e.getSource();
-                    b.setText("X");
+                    b.setText("O");
+                    b.setForeground(Color.black);
+                    totalTry++; find++;
+                    l_success.setText("find : " + find);
+                    l_try.setText("try : " + totalTry);
+                    if (num_mine != find)
+                        l_left.setText("\"" +(num_mine-find)+"\" mines left");
+                    else
+                        l_left.setText("축하합니다!!!!");
                 }
-
             }
             catch (Exception e1) {}
-
         }
-
     }
-
-
+    class MyActionListener2 implements ActionListener { //retry
+        public void actionPerformed(ActionEvent e) {
+            find = 0;
+            totalTry = 0;
+            l_success.setText("find : " + find);
+            l_try.setText("try : " + totalTry);
+            p2.setVisible(false);
+            p1.setVisible(false);
+            p0.setVisible(true);
+            p3.setVisible(false);
+            map = null;
+            buttons = null;
+            out.println("retry");
+        }
+    }
 }
